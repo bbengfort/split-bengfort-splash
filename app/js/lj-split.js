a70166074817044a6c24471354fe96168f58de36
Benjamin Bengfort <benjamin@bengfort.com>
2014-09-08 13:23:56 -0400

    copy update

--- app/js/lj-split.js
+++ app/js/lj-split.js
@@ -13,7 +13,7 @@
 
     // Simple Text Rotator
     $(".rotate").textrotator({
-      animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+      animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
       separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
       speed: 1500 // How many milliseconds until the next word show.
     });
