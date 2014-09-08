c7150c8bf24f51b9ef9090f1511c08fe398a7c8e
Benjamin Bengfort <benjamin@bengfort.com>
2014-09-08 14:26:15 -0400

    favicon

--- app/js/lj-split.js
+++ app/js/lj-split.js
@@ -13,9 +13,9 @@
 
     // Simple Text Rotator
     $(".rotate").textrotator({
-      animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+      animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
       separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-      speed: 1500 // How many milliseconds until the next word show.
+      speed: 2200 // How many milliseconds until the next word show.
     });
 
     // Ajax mailchimp
